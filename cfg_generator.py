15ecd0e4a2989cc8dd717d4936bd1533eb5526ec
Pascal Bühlmann <pascal.buehlmann@stud.hslu.ch>
2021-02-26 15:26:11 +0100

    adding changes

--- cfg_generator.py
+++ cfg_generator.py
@@ -2,12 +2,12 @@ from person import Person
 
 # ANPASSEN
 # ===============================================
-person = Person("Name", "Vorname")
+person = Person("Pascal", "Buehlmann")
 
 # Optional / zum auskommentieren
-# person.set_dialekt("Dialekt")
-# person.set_haustier("Haustier")
-# person.set_lieblingsfarbe("Lieblingsfarbe")
+person.set_dialekt("Äntlibuech")
+person.set_haustier("kein")
+person.set_lieblingsfarbe("türkis")
 
 # ===============================================
 person.toJSON("data/"+person.vorname.lower()+"."+person.name.lower())
