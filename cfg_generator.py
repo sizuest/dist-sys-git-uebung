bd5732a12290c794e914f7cc2e3bf1dec28d70f6
Simon Züst <sizuest@gmail.com>
2021-02-26 15:32:53 +0100

    Neues cfg skrip mit alter Einstellung

--- cfg_generator.py
+++ cfg_generator.py
@@ -2,23 +2,12 @@ from person import Person
 
 # ANPASSEN
 # ===============================================
-<<<<<<< HEAD
-person = Person("Besic", "Amela")
+person = Person("Name", "Vorname")
 
 # Optional / zum auskommentieren
-person.set_dialekt("Luzerner")
-person.set_haustier("Hund")
-person.set_lieblingsfarbe("Pink")
-=======
-
-person = Person("Rogenmoser", "Yves")
-
-# Optional / zum auskommentieren
-person.set_dialekt("Zuger")
-person.set_haustier("Katze")
-person.set_lieblingsfarbe("Blue")
-
->>>>>>> b937c57a182a0a3f3e9deaaffc1211fe241e0a87
+# person.set_dialekt("Dialekt")
+# person.set_haustier("Haustier")
+# person.set_lieblingsfarbe("Lieblingsfarbe")
 
 # ===============================================
 person.toJSON("data/"+person.vorname.lower()+"."+person.name.lower())
