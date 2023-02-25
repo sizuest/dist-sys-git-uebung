e8872076365af4daabd77008bb025b23e7b4ba08
adrianzenklusen <126181029+adrianzenklusen@users.noreply.github.com>
2023-02-25 12:54:35 +0100

    Adrian Zenklusen hinzugefügt

--- cfg_generator.py
+++ cfg_generator.py
@@ -2,11 +2,11 @@ from person import Person
 
 # ANPASSEN
 # ===============================================
-person = Person("Bucher", "Leandro")
+person = Person("Zenklusen", "Adrian")
 # Optional / zum auskommentieren
-person.set_dialekt("Lozaernerduetsch")
-person.set_haustier("Chatze ond Scheldkrote")
-person.set_lieblingsfarbe("Blau")
+person.set_dialekt("Walliserdiitsch")
+person.set_haustier("Nemo")
+person.set_lieblingsfarbe("Gelb")
 person.set_Brille(True)
 # ===============================================
 person.toJSON("data/"+person.vorname.lower()+"."+person.name.lower())
